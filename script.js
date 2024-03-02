cee7375031e8202617fa17c23be8fa42a97b05f1
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-03-02 13:23:36 +0100

    add Armless Kid popup to main

--- script.js
+++ script.js
@@ -14,9 +14,7 @@ function youtubizeParse() {
     const slskyt = document.getElementById("slskyt");
     slskyt.innerHTML = "";
     const input = document.getElementById("input").value;
-    console.log("mon input:", input);
     const arr = parse(input);
-    console.log("mon arr:", arr);
     for (let i = 0; i < arr.length - 1; i++) {
         slskyt.innerHTML += `
             ${arr[i]} <a class="button" href="https://www.youtube.com/results?search_query=${encodeURIComponent(arr[i])}" target="_blank">
